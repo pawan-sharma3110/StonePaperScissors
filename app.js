bc2ce13a32f49d838bc5fec190ba4cfde35d4d05
pawan-sharma3110 <pawansharma93520@gmail.com>
2024-03-18 21:42:56 +0530

    a litle game is ready to play

--- app.js
+++ app.js
@@ -1,12 +1,58 @@
 let userScore = 0;
 let compScore = 0;
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
 
+const getCompChoice = () => {
+  const option = ["rock", "paper", "scissors"];
+  const randIndx = Math.floor(Math.random() * 3);
+  return option[randIndx];
+};
+const drawGame = () => {
+  console.log("Game is draw");
+  msg.innerHTML = "Game was Draw. Play again";
+};
 
+const showWinner = (userWin, userChoice, compChoice) => {
+  if (userWin) {
+    userScore++;
+    userScorePara.innerHTML = userScore;
+    console.log("You win!");
+    msg.innerHTML = `You win! Your ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor = "green";
+  } else {
+    compScore++;
+    compScorePara.innerHTML = compScore;
+    console.log("You lose");
+    msg.innerHTML = `You lose! ${compChoice} beats your ${userChoice}`;
+    msg.style.backgroundColor = "red";
+  }
+};
+
+const playGame = (userChoice) => {
+  console.log("User choice is =", userChoice);
+  const compChoice = getCompChoice();
+  console.log("comp choice is =", compChoice);
+  if (userChoice === compChoice) {
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin, userChoice, compChoice); // Pass userChoice and compChoice here
+  }
+};
 
 choices.forEach((choice) => {
   choice.addEventListener("click", () => {
     let userChoice = choice.getAttribute("id");
-    console.log("Choice was clicked", userChoice);
+    playGame(userChoice);
   });
 });
